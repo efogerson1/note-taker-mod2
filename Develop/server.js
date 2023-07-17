e5723615feaf9f5f71ba5f7df6d32ccc54ae71bf
efogerson1 <efogerson2@gmail.com>
2023-07-17 13:43:09 -0700

    re-ordered js functions

--- Develop/server.js
+++ Develop/server.js
@@ -13,11 +13,6 @@ app.use('/', api);
 
 // GET route for homepage
 
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
-
-
 app.get('/', (req, res)=>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -26,6 +21,10 @@ app.get('/notes', (req, res)=>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 
 app.listen(PORT, () =>
