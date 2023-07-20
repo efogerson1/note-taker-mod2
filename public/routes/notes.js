c795506f713a02d6590e2938764b3ab334e0060a
efogerson1 <efogerson2@gmail.com>
2023-07-20 16:40:29 -0700

    changing routing structure

--- public/routes/notes.js
+++ public/routes/notes.js
@@ -1,6 +1,6 @@
 const notes = require('express').Router();
 const {v4: uuidv4} = require('uuid');
-const {readFromFile, readAndAppend} = require('../../../helpers/fsUtils');
+const {readFromFile, readAndAppend} = require('../../helpers/fsUtils');
 
 
 //GET Route for retrieving notes info
--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use('/api', api);
 
 // GET route for homepage
 
-app.get('/', (req, res)=>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// app.get('/', (req, res)=>
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 app.get('/notes', (req, res)=>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
